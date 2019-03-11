f748e507e84c98cd3e831582cbbec71cdfab9cd3
Martin Kearn <martin.kearn@microsoft.com>
2019-03-11 08:38:20 +0000

    Initial project and secrets.json for application settings

---
+++ src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=guidedsearchbot-Web-Deploy.pubxml -p:Password=gJbpRYMKHti5p39qteheEf4b80gLNccfpeAr8g5ZqWdWua7Ym6fHBmYm90sy
+

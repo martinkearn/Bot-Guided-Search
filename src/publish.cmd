fae92e32b19f01f86f6529df0ab5655730a83bed
Martin Kearn <martin.kearn@microsoft.com>
2019-03-25 10:25:57 +0000

    Remove Azure deployment files

--- src/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=guidedsearchbot-Web-Deploy.pubxml -p:Password=gWbmQk4huaeESu3YpqejdNkHdWPLDcorGylWKduR5JEhnjF8nmafFsk9ZpeR
-

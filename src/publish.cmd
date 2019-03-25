04fea53e2ba476c2d8cc39470c491e538004c419
Martin Kearn <martin.kearn@microsoft.com>
2019-03-25 10:01:02 +0000

    initial project template and move settings to secrets.json

---
+++ src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=guidedsearchbot-Web-Deploy.pubxml -p:Password=gWbmQk4huaeESu3YpqejdNkHdWPLDcorGylWKduR5JEhnjF8nmafFsk9ZpeR
+

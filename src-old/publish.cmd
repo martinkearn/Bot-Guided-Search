84b8cad1d281f557e9483c02db1d7b1c1bb804f8
Martin Kearn <martin.kearn@microsoft.com>
2019-03-29 15:37:20 +0000

    Removed src-old

--- src-old/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=guidedsearchbot-Web-Deploy.pubxml -p:Password=gJbpRYMKHti5p39qteheEf4b80gLNccfpeAr8g5ZqWdWua7Ym6fHBmYm90sy
-
